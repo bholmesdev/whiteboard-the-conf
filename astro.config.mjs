a3d30b657e0bc4db59649d9d4fc3652e925d0c42
bholmesdev <bholmesdev@gmail.com>
2023-10-17 14:25:57 -0400

    chore: svelte config

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,12 +1,14 @@
 import { defineConfig } from "astro/config";
-
 import cloudflare from "@astrojs/cloudflare";
+
+import svelte from "@astrojs/svelte";
 
 // https://astro.build/config
 export default defineConfig({
   output: "server",
   adapter: cloudflare({
     // use miniflare in dev
-    runtime: "local",
+    runtime: "local"
   }),
+  integrations: [svelte()]
 });
